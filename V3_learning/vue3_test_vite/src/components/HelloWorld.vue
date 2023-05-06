690df1e1dcce80c4a34f52de745eeb4cd7506a94
LeroyK <bridieiybnf@gmail.com>
2023-05-06 20:42:52 +0800

    1

--- V3_learning/vue3_test_vite/src/components/HelloWorld.vue
+++ V3_learning/vue3_test_vite/src/components/HelloWorld.vue
@@ -2,18 +2,39 @@
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+
+  <button @click="alertClick"></button>
 </template>
 
 <script>
+
+
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },
   data() {
+    // 响应式语法糖已经废弃
+    // let count2 = $ref()
+
+
+
     return {
       count: 0
     }
-  }
+  },
+
+
+  methods: {
+    // 不再通过$()转换响应式变量。
+    // alertClick(param) {
+    //   count2++;
+    // }
+  },
+
+
+
 }
 </script>
