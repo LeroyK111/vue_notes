3c26437d4b69c42e353a41835d4c4a522810b254
LeroyK <bridieiybnf@gmail.com>
2023-05-29 21:47:43 +0800

    1

--- V3_learning/my-vue-ts/src/components/HelloWorld.vue
+++ V3_learning/my-vue-ts/src/components/HelloWorld.vue
@@ -1,42 +1,55 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onActivated, onDeactivated, ref } from 'vue'
+import Son from "./Son.vue"
+import Girl from "./Girl.vue"
+
+defineProps<{msg: string}>()
+const showHide = ref("Son")
+
+/**
+ * @author Leroy
+ * 缓存的时机：
+ * !1> 进入列表页后清除缓存 | 点击链接跳转前清除缓存
+ * ?2> 状态管理实现缓存
+ * *3> 页面弹窗实现缓存
+ * todo4> 父子路由实现缓存
+*/
 
 
-defineProps<{ msg: string }>()
 
-const count = ref(0)
+const tabs: any = {
+  Son,
+  Girl
+}
 
+
+onActivated(() => {
+  // 调用时机为首次挂载
+  // 以及每次从缓存中被重新插入时
+  console.log("111");
+  
+})
+
+onDeactivated(() => {
+  // 在从 DOM 上移除、进入缓存
+  // 以及组件卸载时调用
+  console.log("222");
+  
+})
 
 
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  
+  A:<input type="radio" value="Son" v-model="showHide">
+  B:<input type="radio" value="Girl" v-model="showHide">
+  <!-- keepAlive 只能缓存动态组件的状态 -->
+  <KeepAlive :include="['Girl']" :exclude="['Son']" :max="3">
+    <component :is="tabs[showHide]"></component>
+  </KeepAlive>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
 </style>
---
+++ V3_learning/my-vue-ts/src/components/Son.vue
@@ -0,0 +1,16 @@
+<template>
+  <br>
+  <span>your select Son</span>
+  <br>
+  <input type="number" :value="count">
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0)
+
+
+</script>
+<style lang="">
+
+</style>
