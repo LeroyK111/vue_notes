26325ea493e6ac492662229205f6f2021dc1f7f6
LeroyK <785632486@qq.com>
2023-05-31 13:15:09 +0800

    1

---
+++ V3_learning/my-vue-ts/src/components/Girl.vue
@@ -0,0 +1,16 @@
+<template>
+  <br>
+  <span>your select Girl</span>
+  <br>
+  <input type="text" :value="strs">
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const strs = ref("")
+
+
+</script>
+<style lang="">
+
+</style>
