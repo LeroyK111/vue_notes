d9d2e5c1807153ce77c593f4c636387ab57af4cf
LeroyK <785632486@qq.com>
2023-06-02 17:45:23 +0800

    1

--- V3_learning/my-vue-ts/src/App.vue
+++ V3_learning/my-vue-ts/src/App.vue
@@ -1,7 +1,41 @@
-<script setup lang="ts">
+<script setup lang="ts" generic="T">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+// !引入泛型组件
+defineProps<{
+  items: T[]
+  selected: T
+}>()
+
+// 面向宏支持类型导入和复杂类型
+// defineProps<Props & { extraProp?: string }>()
+
+
+// 改进 defineEmits
+  const emit = defineEmits<{
+  foo: [id: number]
+  bar: [name: string, ...rest: any[]]
+}>()
+
+
+// 设置插槽内容
+defineSlots<{
+  default?: (props: { msg: string }) => any
+  item?: (props: { id: number }) => any
+}>()
+
+
+// 实验性功能，解构赋值，但是不失去数据包装
+const { msg = 'hello' } = defineProps(['msg'])
+
+
+// 双向绑定
+const modelValue = defineModel()
+console.log(modelValue.value)
+
+
 </script>
 
 <template>
