ec3e2843691824a56f9628996d19290e0825c67b
LeroyK <785632486@qq.com>
2023-06-28 01:07:58 +0800

    2023年6月28日

--- V3_learning/vue3_test_vite/src/App.vue
+++ V3_learning/vue3_test_vite/src/App.vue
@@ -6,6 +6,15 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+// 对于array能监听到数组变换的函数
+  //push()
+// pop()
+// shift()
+// unshift()
+// splice()
+// sort()
+// reverse()
+
 export default {
   name: 'App',
   components: {
