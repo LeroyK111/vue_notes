e9bf9a8f0f739caba64100c84fff84749981367b
LeroyK <bridieiybnf@gmail.com>
2024-03-08 11:24:39 +0800

    1

---
+++ V3_learning/testH/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, reactive, h, toRefs, toRef, computed } from "vue";
+
+const option = reactive({
+  innerText: "你好",
+});
+
+const backgroundColor = ref("red");
+
+const click = (e) => {
+  backgroundColor.value = 'green';
+  console.log(e);
+};
+
+const dom = h("div", {
+  style: {
+    backgroundColor: backgroundColor.value,
+    width: "100px",
+    height: "100px",
+  },
+  innerText: option.innerText,
+  onClick: click,
+});
+</script>
+
+<template>
+  <dom></dom>
+  <button @click="click">点我</button>
+</template>
+
+<style scoped></style>
