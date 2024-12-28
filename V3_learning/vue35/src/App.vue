c89e8eabfb58477ec76a011029a1e77de09c75e3
LeroyK <bridieiybnf@gmail.com>
2024-12-28 12:37:35 +0800

    1

---
+++ V3_learning/vue35/src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+/**
+ * @author Leroy
+
+使用alien-signals构建响应式基础
+import { signal, computed, effect } from 'alien-signals';
+
+const count = signal(1);
+const doubleCount = computed(() => count.get() * 2);
+
+effect(() => {
+  console.log(`Count is: ${count.get()}`);
+}); // Console: Count is: 1
+
+console.log(doubleCount.get()); // 2
+
+count.set(2); // Console: Count is: 2
+
+console.log(doubleCount.get()); // 4
+*/
+
+/**
+ * @author Leroy
+ * import { signal, effectScope } from 'alien-signals';
+
+const count = signal(1);
+const scope = effectScope();
+
+scope.run(() => {
+  effect(() => {
+    console.log(`Count in scope: ${count.get()}`);
+  }); // Console: Count in scope: 1
+
+  count.set(2); // Console: Count in scope: 2
+});
+
+scope.stop();
+
+count.set(3); // No console output
+*/
+</script>
+
+<template>
+  <div>asdasd</div>
+  <li>1323</li>
+  <a href="https://www.google.com" target="_blank">click me</a>
+  <!-- <div>{{ count }}</div> -->
+</template>
+
+<style scoped>
+
+</style>
