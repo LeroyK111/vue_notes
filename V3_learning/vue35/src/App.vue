c07b90d569f4f6e2d6fd0959741856721f2da970
LeroyK <bridieiybnf@gmail.com>
2025-02-18 23:18:59 +0800

    add: useId知识

--- V3_learning/vue35/src/App.vue
+++ V3_learning/vue35/src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useId, watch } from "vue";
+
 /**
  * @author Leroy
 
@@ -38,6 +40,11 @@ scope.stop();
 
 count.set(3); // No console output
 */
+
+
+// 其实原理很简单，就是调用 getCurrentInstance 这个，这个 API 会返回当前 Vue 实例的信息对象，而这个信息对象身上有一个 ids 的数组，而 useId 就是根据这个数组去生成唯一 ID 的
+const id = useId();
+console.log(id);
 </script>
 
 <template>
@@ -45,8 +52,8 @@ count.set(3); // No console output
   <li>1323</li>
   <a href="https://www.google.com" target="_blank">click me</a>
   <!-- <div>{{ count }}</div> -->
+  <label :for="id">名称</label>
+  <input type="text" :id="id" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- V3_learning/vue35/src/main.ts
+++ V3_learning/vue35/src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// 在此应用程序中为通过useId（）生成的所有ID配置前缀。
+app.config.idPrefix = "useIdTest"
+app.mount('#app')
+
+
