cdc5c23944c7e37440fa645280d17286eef9c0ff
LeroyK <bridieiybnf@gmail.com>
2025-08-29 01:08:00 +0800

    11

---
+++ V3_learning/vue36/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+ <div>
+  vue3.6 版本 新特性
+  <br>
+
+  <section>
+   Vapor mode
+   使用真实dom构建vue项目
+  </section>
+
+ </div>
+</template>
+
+<style scoped></style>
